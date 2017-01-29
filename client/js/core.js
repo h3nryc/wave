d1b94594c901e4905499b18419ba5dfe8eaf80da
Tom Lister <tomlister3@gmail.com>
2017-01-29 20:47:26 +1100

    Added client side core technologies and server side technologies
    Client: discovery scanner, xhr polling, cmdsend.
    Server: discovery, xhr polling server, cmdrecv, tick.
    
    What is tick?
    Tick is a system to automatically run jobs, commands, code etc and
    either send to a client via xhr polling (dataqueue) or return nothing.

---
+++ client/js/core.js
@@ -0,0 +1,51 @@
+var discoveredDevices = [];
+var activeDevice = {};
+$.ajaxSetup({ timeout: 4000 });
+function waveFinder() {
+//IP Adresses ending in 0 or 255 are not scanned, because they cannot be assigned. Therefore the scanned range is 1 - 254.
+  for (i = 1; i < 254; i++) {
+    //?rand="+Date.now(); is used to prevent cached data.
+    $.get( "http://192.168.1."+i+":3000/api/discover/?rand="+Date.now(), function( data ) {
+      if (data.device == "WaveDevice") {
+        discoveredDevices.push(data);
+        var posn = discoveredDevices.length -1;
+        $(".devices").append("<div class='device' onclick='connect("+posn+");' ><i class='material-icons'>speaker</i><div class='devicename'>"+data.name+"</div></div>");
+      }
+    });
+  }
+}
+//example command { 	"cmd": { 		"type": "tick", 		"command": "rtr_unixtime" 	} }
+function sendcmd(com) {
+  $.get( "http://"+activeDevice.ip+":3000/api/cmd/?c="+com+"&rand="+Date.now(), function( data ) {
+  });
+}
+function pushconsole(input) {
+  $(".console").text($(".console").text()+input+"\n");
+  var textarea = document.getElementById('console');
+  textarea.scrollTop = textarea.scrollHeight;
+}
+function loadDevice() {
+  $(".app").append('<div class="page" id="device"><div class="pagename">'+activeDevice.name+'</div><br><textarea rows="20" cols="50" class="console" id="console"></textarea><br><input type="text" class="commandbox"></input><div onclick="sendcmd($('+"'.commandbox'"+').val());" class="submitbutton">Send</div></div>');
+}
+function connect(pos) {
+  activeDevice = discoveredDevices[pos];
+  poll();
+  document.getElementById("init").style.display = "none";
+  loadDevice();
+}
+
+function poll() {
+  function timeout() {
+    setTimeout(function () {
+      $.get( "http://"+activeDevice.ip+":3000/api/poll/?rand="+Date.now(), function( data ) {
+        for (i = 0; i < data.length; i++) {
+          if (data[i].type == "cmdrtr") {
+            pushconsole(data[i].value)
+          }
+        }
+      });
+        timeout();
+    }, 1000);
+  }
+  timeout();
+}
--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var server = app.listen(process.env.PORT || 3000);
 app.use(express.static('dashboard'));
 var router = express.Router();
 //DB SET UP
-
+var ticks = [];
 var Datastore = require('nedb')
-, tracks = new Datastore({ filename: './tracks.json', autoload: true });
-
+, tracks = new Datastore({ filename: './tracks.json', autoload: true }), info = new Datastore({ filename: './info.json', autoload: true });
+var dataqueue = [];
 var addTrack = function(req,res,next) {
   var doc = { unix: Date.now()
                , id: req.params.uid
@@ -21,5 +21,54 @@ var addTrack = function(req,res,next) {
     }
   });
 }
-
+function tickmanager() {
+  function timeout() {
+    setTimeout(function () {
+      console.log("tick run");
+      console.log(ticks);
+      for (i = 0; i < ticks.length; i++) {
+        console.log("ok");
+        if (ticks[i] == "rtr_unixtime") {
+          console.log({"type":"cmdrtr","value":"SystemTime: "+Date.now()});
+          dataqueue.push({"type":"cmdrtr","value":"SystemTime: "+Date.now()});
+        }
+      }
+      timeout();
+    }, 200);
+  }
+  timeout();
+}
+function addtick(com) {
+  ticks.push(com);
+}
+var discover = function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.send({
+	"name": "Test Wave",
+	"ip": "192.168.1.4",
+	"device": "WaveDevice"
+});
+}
+var poll = function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.send(dataqueue);
+    dataqueue = [];
+}
+var cmd = function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    console.log(req.query.c);
+    var jcmd = JSON.parse(req.query.c);
+    if (jcmd.cmd.type == "tick") {
+      console.log("cmd check tick");
+      addtick(jcmd.cmd.command);
+    }
+    res.send("done");
+}
 app.get('/api/addTrack/:uid',addTrack);
+app.get('/api/discover/', discover);
+app.get('/api/poll/', poll);
+app.get('/api/cmd/', cmd);
+tickmanager();
